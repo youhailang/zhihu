11f24f0ed57bc44a8758728f4c0f5c7a2bfca3e8
burna <burna@192.168.0.110>
2015-08-18 00:58:43 +0800

    a

--- src/test/java/com/youhl/zhihu/service/TestHtmlUnit.java
+++ src/test/java/com/youhl/zhihu/service/TestHtmlUnit.java
@@ -1,30 +1,43 @@
 package com.youhl.zhihu.service;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 
 import org.junit.Test;
 
+import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
-import com.gargoylesoftware.htmlunit.NicelyResynchronizingAjaxController;
 import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
+import com.gargoylesoftware.htmlunit.html.HtmlButton;
+import com.gargoylesoftware.htmlunit.html.HtmlForm;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlPasswordInput;
+import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
 
 public class TestHtmlUnit {
   @Test
-  public void test1() {
-    WebClient webClient = new WebClient();
-    webClient.setJavaScriptEnabled(true);
-    webClient.setCssEnabled(false);
-    webClient.setAjaxController(new NicelyResynchronizingAjaxController());
-    webClient.setTimeout(Integer.MAX_VALUE);
-    webClient.setThrowExceptionOnScriptError(false);
-    HtmlPage rootPage;
-    try {
-      rootPage = webClient.getPage("http://tt.mop.com/read_14304066_1_0.html");
-      System.out.println(rootPage.asXml());
-    } catch (FailingHttpStatusCodeException | IOException e) {
-      e.printStackTrace();
-    }
+  public void test1() throws FailingHttpStatusCodeException, MalformedURLException, IOException {
+    final WebClient webClient = new WebClient(BrowserVersion.CHROME);
+    webClient.getOptions().setCssEnabled(false);
+    webClient.getOptions().setJavaScriptEnabled(true);
+    // webClient.setJavaScriptEnabled(true);
+    // webClient.setCssEnabled(false);
+    // webClient.setAjaxController(new NicelyResynchronizingAjaxController());
+    // webClient.setTimeout(Integer.MAX_VALUE);
+    // webClient.setThrowExceptionOnScriptError(false);
+    HtmlPage page = webClient.getPage("http://www.zhihu.com/");
+    HtmlAnchor anchor = page.getAnchorByHref("#signin");
+    page = anchor.click();
+    HtmlForm form = page.getForms().get(0);
+    HtmlTextInput account = form.getInputByName("account");
+    HtmlPasswordInput password = form.getInputByName("password");
+    HtmlButton submit = (HtmlButton) form.getByXPath("div/button").get(0);
 
+    account.setValueAttribute("906669319@qq.com");
+    password.setValueAttribute("qazwsx");
+    page = submit.click();
+    System.out.println("登陆:" + page.asXml());
+    webClient.closeAllWindows();
   }
 }
